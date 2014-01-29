80368c4bb41530c87c24df95a1d318f4c7e95877
Simen Haugerud Granlund <simen@hgranlund.com>
2014-01-29 15:40:29 +0100

    added correctnes test in knn_brute_force

--- tests/kNN/test_kNN-brute-force.cpp
+++ tests/kNN/test_kNN-brute-force.cpp
@@ -1,12 +1,5 @@
 #include "gtest/gtest.h"
 
-// Constants used by the program
-#define MAX_PITCH_VALUE_IN_BYTES       262144
-#define MAX_TEXTURE_WIDTH_IN_BYTES     65536
-#define MAX_TEXTURE_HEIGHT_IN_BYTES    32768
-#define MAX_PART_OF_FREE_MEMORY_USED   0.9
-#define BLOCK_DIM                      16
-
 
 #include <knn.h>
 
@@ -16,68 +9,56 @@
 #include <math.h>
 #include <time.h>
 
+TEST(knn_tests, test_knn_brute_force_give_rigth_result_with_6553_points){
 
-TEST(knn_tests, knn_complete_test){
-
-
-
-
-  // Variables and parameters
   float* ref;                 // Pointer to reference point array
   float* query;               // Pointer to query point array
   float* dist;                // Pointer to distance array
   int*   ind;                 // Pointer to index array
-  int    ref_nb     = 4064;    // Reference point number, max=65535
-  int    query_nb   = 1;   // Query point number,     max=65535
-  int    dim        = 3;      // Dimension of points
+  int    ref_nb;              // Reference point number, max=65535
+  int    query_nb;            // Query point number,     max=65535
+  int    dim;                 // Dimension of points
   int    k          = 20;     // Nearest neighbors to consider
   int    iterations = 1;
   int    i;
 
-  // Memory allocation
+  char fileName[] = "data/knn_brute_force_6553_ref_points_1_query_point.data";
+  FILE* file = fopen(fileName, "rb");
+
+  fread(&ref_nb, sizeof(int), 1, file);
+  fread(&query_nb, sizeof(int), 1, file);
+  fread(&dim, sizeof(int), 1, file);
+
   ref    = (float *) malloc(ref_nb   * dim * sizeof(float));
   query  = (float *) malloc(query_nb * dim * sizeof(float));
   dist   = (float *) malloc(query_nb * k * sizeof(float));
   ind    = (int *)   malloc(query_nb * k * sizeof(float));
 
-  srand(time(NULL));
-  for (i=0 ; i<ref_nb   * dim ; i++){
-    ref[i]    = (float)rand() / (float)RAND_MAX;
+  for (int count = 0; count < ref_nb*dim; count++)
+  {
+    fread(&ref[count], sizeof(float), 1, file);
   }
-  for (i=0 ; i<query_nb * dim ; i++){
-    query[i]  = (float)rand() / (float)RAND_MAX;
+  for (int count = 0; count < query_nb*dim; count++)
+  {
+    fread(&query[count], sizeof(float), 1, file);
   }
 
-  // cudaEvent_t start, stop;
-  // cudaEventCreate(&start);
-  // cudaEventCreate(&stop);
-  // float elapsed_time;
+  fclose(file);
 
-  printf("Number of reference points      : %6d\n", ref_nb  );
-  printf("Number of query points          : %6d\n", query_nb);
-  printf("Dimension of points             : %4d\n", dim     );
-  printf("Number of neighbors to consider : %4d\n", k       );
-  printf("Processing kNN search           :\n"                );
-
-  // cudaEventRecord(start, 0);
   for (i=0; i<iterations; i++){
     knn(ref, ref_nb, query, query_nb, dim, k, dist, ind);
   }
-  // cudaEventRecord(stop, 0);
-  // cudaEventSynchronize(stop);
-  // cudaEventElapsedTime(&elapsed_time, start, stop);
-  // printf(" done in %f s for %d iterations (%f s by iteration)\n", elapsed_time/1000, iterations, elapsed_time/(iterations*1000));
 
-  // cudaEventDestroy(start);
-  // cudaEventDestroy(stop);
+  int correct_ind[] = {3261, 2799, 5752, 1837, 522, 5065, 5410, 1915, 2618, 627, 6095, 305, 3375, 269, 6180, 4963, 2216, 3393, 31, 5061};
+  for (int i = 0; i < k; ++i)
+  {
+    ASSERT_EQ(ind[i], correct_ind[i]);
+  }
 
   free(ind);
   free(dist);
   free(query);
   free(ref);
   SUCCEED();
-
-
-
 }
 
