af81b0a8d5d23517cfa53f50eede88cbd75b4a71
Simen Haugerud Granlund <simen@hgranlund.com>
2014-02-03 11:43:46 +0100

    did some modification to re-implementation of knn brute force

--- src/kNN/kNN-brute-force.cu
+++ src/kNN/kNN-brute-force.cu
@@ -6,11 +6,11 @@
 #include <math.h>
 #include <cuda.h>
 #include <time.h>
-
+#include "bitonic-sort-serial.c"
 
 __constant__  float query_dev[3];
 
-__global__ void cuComputeDistanceGlobal( float* ref, int ref_nb , int dim,  float* dist){
+__global__ void cuComputeDistanceGlobal( float* ref, int ref_nb , int dim,  float* dist, int* ind){
 
   // restiction: dim=3
   float dx,dy,dz;
@@ -21,22 +21,11 @@ __global__ void cuComputeDistanceGlobal( float* ref, int ref_nb , int dim,  floa
     dy=ref[index + 1] - query_dev[1];
     dz=ref[index + 2] - query_dev[2];
     dist[index/dim] = (dx*dx)+(dy*dy)+(dz*dz);
+    ind[index/dim] = index/dim;
     index += gridDim.x * dim;
   }
 }
 
-__global__ void cuCreateIndex(int *ind, int ind_nb){
-  unsigned int index = blockIdx.x;
-  while (index < ind_nb){
-    ind[index] = index;
-    index += blockDim.x;
-  }
-}
-
-__global__ void cuInsertionSort(float *dist, int *ind, int dist_nb, int k){
-
-
-}
 
 __global__ void cuParallelSqrt(float *dist, int k){
   unsigned int xIndex = blockIdx.x;
@@ -61,17 +50,35 @@ void knn_brute_force(float* ref_host, int ref_nb, float* query_host, int dim, in
   cudaMalloc( (void **) &ind_dev, ref_nb * size_of_int);
   cudaMalloc( (void **) &ref_dev, ref_nb * size_of_float * dim);
 
+
   cudaMemcpy(ref_dev, ref_host, ref_nb*dim*size_of_float, cudaMemcpyHostToDevice);
   cudaMemcpyToSymbol(query_dev, query_host, dim*size_of_float);
 
-  cuComputeDistanceGlobal<<<256,1>>>(ref_dev, ref_nb, dim, dist_dev);
-
-  // cuCreateIndex<<<256,1>>>(ind_dev, ref_nb);
+  cuComputeDistanceGlobal<<<256,1>>>(ref_dev, ref_nb, dim, dist_dev, ind_dev);
 
   // cuParallelSqrt<<<k,1>>>(dist_dev, k);
-
   cudaMemcpy(dist_host, dist_dev, k*size_of_float, cudaMemcpyDeviceToHost);
   cudaMemcpy(ind_host,  ind_dev,  k*size_of_int, cudaMemcpyDeviceToHost);
+
+  printf("\n[");
+  for (int i = 0; i < k; ++i)
+  {
+    printf("%d, ",ind_host[i] );
+  }
+  printf("]\n");
+
+
+  printf("\n[");
+  for (int i = 0; i < k; ++i)
+  {
+    printf("%f, ",dist_host[i]);
+  }
+  printf("]\n");
+
+
+
+
+  // bitonic_sort_serial(dist_host, k);
 
   cudaFree(ref_dev);
   cudaFree(ind_dev);
