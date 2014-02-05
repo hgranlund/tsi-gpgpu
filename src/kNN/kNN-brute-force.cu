23a8b0af8cf5bd846b422070068d5186f7018213
Simen Haugerud Granlund <simen@hgranlund.com>
2014-02-05 14:22:39 +0100

    made bitonic sort in the brute force algorithm

--- src/kNN/kNN-brute-force.cu
+++ src/kNN/kNN-brute-force.cu
@@ -6,24 +6,77 @@
 #include <math.h>
 #include <cuda.h>
 #include <time.h>
-#include "bitonic-sort-serial.c"
+#include <assert.h>
+#define SHARED_SIZE_LIMIT 512U
 
+__device__ void cuCompare(float &distA, int &indA, float &distB, int &indB, int dir)
+{
+  float f;
+  int i;
+  if ((distA  > distB) == dir)
+  {
+    f = distA;
+    distA  = distB;
+    distB = f;
+    i = indA;
+    indA = indB;
+    indB = i;
+  }
+}
 __constant__  float query_dev[3];
 
 __global__ void cuComputeDistanceGlobal( float* ref, int ref_nb , int dim,  float* dist, int* ind){
 
-  // restiction: dim=3
   float dx,dy,dz;
 
-  int index = blockIdx.x * dim;
+  int index = blockIdx.x;
   while (index < ref_nb){
-    dx=ref[index] - query_dev[0];
-    dy=ref[index + 1] - query_dev[1];
-    dz=ref[index + 2] - query_dev[2];
-    dist[index/dim] = (dx*dx)+(dy*dy)+(dz*dz);
-    ind[index/dim] = index/dim;
-    index += gridDim.x * dim;
+    dx=ref[index*dim] - query_dev[0];
+    dy=ref[index*dim + 1] - query_dev[1];
+    dz=ref[index*dim + 2] - query_dev[2];
+    dist[index] = (dx*dx)+(dy*dy)+(dz*dz);
+    ind[index] = index;
+    index += gridDim.x;
   }
+}
+
+
+__global__ void cuBitonicSort(float* dist, int* ind, int n,int dir){
+
+  int blockoffset = blockIdx.x * blockDim.x *2;
+  dist+=blockoffset;
+  ind+=blockoffset;
+
+  for (uint size = 2; size <= blockDim.x*2; size <<= 1)
+  {
+    uint ddd = dir ^ ((threadIdx.x & (size / 2)) != 0);
+    for (uint stride = size / 2; stride > 0; stride >>= 1)
+    {
+      __syncthreads();
+      uint pos = 2 * threadIdx.x - (threadIdx.x & (stride - 1));
+      cuCompare(dist[pos], ind[pos], dist[pos + stride], ind[pos + stride],ddd);
+    }
+  }
+
+  uint ddd = blockIdx.x & 1;
+  {
+    for (uint stride = blockDim.x; stride > 0; stride >>= 1)
+    {
+      __syncthreads();
+      uint pos = 2 * threadIdx.x - (threadIdx.x & (stride - 1));
+      cuCompare(dist[pos], ind[pos], dist[pos + stride], ind[pos + stride],ddd);
+    }
+  }
+}
+
+__global__ void cuBitonicMergeGlobal(float* dist, int* ind, int n, int size, int stride ,int dir)
+{
+  uint global_comparatorI = blockIdx.x * blockDim.x + threadIdx.x;
+  uint        comparatorI = global_comparatorI & (n / 2 - 1);
+
+  int ddd = dir ^ ((comparatorI & (size / 2)) != 0);
+  int pos = 2 * global_comparatorI - (global_comparatorI & (stride - 1));
+  cuCompare(dist[pos], ind[pos], dist[pos + stride], ind[pos + stride],ddd);
 }
 
 
@@ -35,6 +88,60 @@ __global__ void cuParallelSqrt(float *dist, int k){
 }
 
 
+__global__ void cuBitonicMergeShared(float* dist, int* ind, int n, int size, int dir)
+{
+
+  int blockoffset = blockIdx.x * blockDim.x *2;
+  dist+=blockoffset;
+  ind+=blockoffset;
+  uint comparatorI = (blockIdx.x * blockDim.x + threadIdx.x) & ((n / 2) - 1);
+  uint ddd = dir ^ ((comparatorI & (size / 2)) != 0);
+  for (uint stride = SHARED_SIZE_LIMIT / 2; stride > 0; stride >>= 1)
+  {
+    __syncthreads();
+    uint pos = 2 * threadIdx.x - (threadIdx.x & (stride - 1));
+    cuCompare(dist[pos], ind[pos], dist[pos + stride], ind[pos + stride],ddd);
+  }
+}
+
+void bitonic_sort(float* dist_dev, int* ind_dev, int n){
+  int  blockCount = n / SHARED_SIZE_LIMIT;
+  int threadCount = SHARED_SIZE_LIMIT/ 2;
+  int dir = 1;
+  blockCount = max(1, blockCount);
+  threadCount = min(SHARED_SIZE_LIMIT, threadCount);
+  cuBitonicSort<<<blockCount, 2>>>(dist_dev,ind_dev, n, dir);
+  if (blockCount>1)
+  {
+    for (uint size = 2 * SHARED_SIZE_LIMIT; size <= n; size <<= 1){
+      for (uint stride = size / 2; stride > 0; stride >>= 1){
+        if (stride >= SHARED_SIZE_LIMIT)
+        {
+          cuBitonicMergeGlobal<<<blockCount, threadCount>>>(dist_dev,ind_dev, n, size, stride, dir);
+        }
+        else
+        {
+          cuBitonicMergeShared<<<blockCount, threadCount>>>(dist_dev,ind_dev, n, size, dir);
+          break;
+        }
+      }
+    }
+  }
+}
+
+uint factorRadix2(uint *log2L, uint L)
+{
+  if (!L)
+  {
+    *log2L = 0;
+    return 0;
+  }
+  else
+  {
+    for (*log2L = 0; (L & 1) == 0; L >>= 1, *log2L++);
+      return L;
+  }
+}
 
 void knn_brute_force(float* ref_host, int ref_nb, float* query_host, int dim, int k, float* dist_host, int* ind_host){
 
@@ -46,41 +153,27 @@ void knn_brute_force(float* ref_host, int ref_nb, float* query_host, int dim, in
   int          *ind_dev;
 
 
+  uint log2L;
+  uint factorizationRemainder = factorRadix2(&log2L, ref_nb);
+  assert(factorizationRemainder == 1);
+
   cudaMalloc( (void **) &dist_dev, ref_nb * size_of_float);
   cudaMalloc( (void **) &ind_dev, ref_nb * size_of_int);
   cudaMalloc( (void **) &ref_dev, ref_nb * size_of_float * dim);
 
-
   cudaMemcpy(ref_dev, ref_host, ref_nb*dim*size_of_float, cudaMemcpyHostToDevice);
   cudaMemcpyToSymbol(query_dev, query_host, dim*size_of_float);
-
   cuComputeDistanceGlobal<<<256,1>>>(ref_dev, ref_nb, dim, dist_dev, ind_dev);
+  bitonic_sort(dist_dev,ind_dev, ref_nb);
+  cuParallelSqrt<<<k,1>>>(dist_dev, k);
 
-  // cuParallelSqrt<<<k,1>>>(dist_dev, k);
   cudaMemcpy(dist_host, dist_dev, k*size_of_float, cudaMemcpyDeviceToHost);
   cudaMemcpy(ind_host,  ind_dev,  k*size_of_int, cudaMemcpyDeviceToHost);
-
-  printf("\n[");
-  for (int i = 0; i < k; ++i)
-  {
-    printf("%d, ",ind_host[i] );
-  }
-  printf("]\n");
-
-
-  printf("\n[");
-  for (int i = 0; i < k; ++i)
-  {
-    printf("%f, ",dist_host[i]);
-  }
-  printf("]\n");
-
-
-
-
-  // bitonic_sort_serial(dist_host, k);
 
   cudaFree(ref_dev);
   cudaFree(ind_dev);
   cudaFree(query_dev);
 }
+
+
+
--- src/kNN/kNN-brute-force.cuh
+++ src/kNN/kNN-brute-force.cuh
@@ -2,12 +2,10 @@
 #define _KNN_BRUTE_FORCE_
 
 __global__ void cuComputeDistanceGlobal( float* ref, int ref_nb , float* query, int dim,  float* dist);
-
-// __global__ void cuInsertionSort(float *dist, , int *ind, int width, int dim, int k){
-
+__global__ void cuBitonicSort(float* dist, int* ind, int n,int dir);
 __global__ void cuParallelSqrt(float *dist, int k);
 
-
+void bitonic_sort(float* dist_dev, int* ind_dev, int n);
 void knn_brute_force(float* ref_host, int ref_nb, float* query_host, int dim, int k, float* dist_host, int* ind_host);
 
 
