4c60f128541652a4c6d189cd97b1d54ceec126da
Simen Haugerud Granlund <simen@hgranlund.com>
2014-02-23 13:31:04 +0100

    moved Distance stuct to public header

--- src/kNN/kernels/reduction-mod.cuh
+++ src/kNN/kernels/reduction-mod.cuh
@@ -1,34 +1,9 @@
 #ifndef _REDUCTION_MOD
 #define _REDUCTION_MOD
+#include <knn_gpgpu.h>
 
 
-struct Distance {
- unsigned int index;
- float value;
 
- __host__  __device__  Distance& operator=(volatile Distance& a)
- {
-  index=a.index;
-  value=a.value;
-  return *this;
-}
-
-__host__  __device__  volatile Distance& operator=(Distance& a)
-{
-  index=a.index;
-  value=a.value;
-  return *this;
-}
-
-__host__ __device__ volatile Distance& operator=(volatile Distance& a) volatile
-{
-  index=a.index;
-  value=a.value;
-  return *this;
-}
-
-
-};
 
 void dist_min_reduce(Distance* dist_dev, unsigned int n);
 
