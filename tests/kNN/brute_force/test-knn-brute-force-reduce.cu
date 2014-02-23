4c569a886a387dbe3602d1f7fe03374529a93af3
Simen Haugerud Granlund <simen@hgranlund.com>
2014-02-23 14:41:18 +0100

    added time test in knn-bruite-force-reduce

--- tests/kNN/brute_force/test-knn-brute-force-reduce.cu
+++ tests/kNN/brute_force/test-knn-brute-force-reduce.cu
@@ -7,13 +7,16 @@
 
 #include <stdio.h>
 #include <cuda.h>
+#include <helper_functions.h>
+#include <helper_cuda.h>
+#define checkCudaErrors(val)           check ( (val), #val, __FILE__, __LINE__ )
 
 
 TEST(knn_brute_force, test_knn_reduce_correctness){
 
-  float* ref;
-  float* query;
-  Distance* dist;
+  float *ref, *dist;
+  float *query;
+  int *ind;
   unsigned int    ref_nb = 131072;
   unsigned int    query_nb = 1;
   unsigned int    dim=3;
@@ -23,7 +26,8 @@ TEST(knn_brute_force, test_knn_reduce_correctness){
 
   ref    = (float *) malloc(ref_nb   * dim * sizeof(float));
   query  = (float *) malloc(query_nb * dim * sizeof(float));
-  dist   = (Distance *) malloc(query_nb * k * sizeof(Distance));
+  dist  = (float *) malloc(k * sizeof(float));
+  ind  = (int *) malloc(k * sizeof(float));
 
   for (unsigned int count = 0; count < ref_nb*dim; count++)
   {
@@ -35,15 +39,72 @@ TEST(knn_brute_force, test_knn_reduce_correctness){
   }
 
   for (i=0; i<iterations; i++){
-    knn_brute_force_reduce(ref, ref_nb, query, dim, k, dist);
+    knn_brute_force_reduce(ref, ref_nb, query, dim, k, dist, ind);
   }
 
   for (unsigned int i = 0; i < k; ++i)
   {
-    ASSERT_EQ(dist[i].index, ref_nb-1-i)<< "Faild with i = "<<i << " and n = " << ref_nb;;
+    ASSERT_EQ(ind[i], ref_nb-1-i) << "Faild with i = "<<i << " and n = " << ref_nb;;
   }
 
   free(dist);
+  free(ind);
+  free(query);
+  free(ref);
+}
+
+TEST(knn_brute_force, test_knn_reduce_time){
+
+  float *ref, *dist;
+  float *query;
+  int *ind;
+  unsigned int    ref_nb = 131072;
+  unsigned int    query_nb = 1;
+  unsigned int    dim=3;
+  unsigned int    k          = 50;
+
+  ref    = (float *) malloc(ref_nb   * dim * sizeof(float));
+  query  = (float *) malloc(query_nb * dim * sizeof(float));
+  dist  = (float *) malloc(k * sizeof(float));
+  ind  = (int *) malloc(k * sizeof(float));
+
+  for (unsigned int count = 0; count < ref_nb*dim; count++)
+  {
+    ref[count] = ref_nb*dim-count;
+  }
+  for (unsigned int count = 0; count < query_nb*dim; count++)
+  {
+    query[count] = 0;
+  }
+
+  cudaEvent_t start, stop;
+  unsigned int bytes = ref_nb * (sizeof(float));
+  checkCudaErrors(cudaEventCreate(&start));
+  checkCudaErrors(cudaEventCreate(&stop));
+  float elapsed_time=0;
+
+  checkCudaErrors(cudaEventRecord(start, 0));
+
+  knn_brute_force_reduce(ref, ref_nb, query, dim, k, dist, ind);
+
+
+  checkCudaErrors(cudaEventRecord(stop, 0));
+  cudaEventSynchronize(start);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&elapsed_time, start, stop);
+  elapsed_time = elapsed_time * 1e-3;
+  float throughput = 1.0e-9 * ((float)bytes)/elapsed_time;
+  printf("kNN-brute-force-reduce, Throughput = %.4f GB/s, Time = %.5f s, Size = %u Elements, k = %u NumDevsUsed = %d\n",
+   throughput, elapsed_time, ref_nb,k, 1);
+
+
+  for (unsigned int i = 0; i < k; ++i)
+  {
+    ASSERT_EQ(ind[i], ref_nb-1-i) << "Faild with i = "<<i << " and n = " << ref_nb;;
+  }
+
+  free(dist);
+  free(ind);
   free(query);
   free(ref);
 }
