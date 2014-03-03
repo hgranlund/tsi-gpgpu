0dfc5fe458fdf81677894f4d07efec67eb78c1ae
Simen Haugerud Granlund <simen@hgranlund.com>
2014-03-03 13:54:57 +0100

    kd-tree-naive is working with bitonic

--- src/kNN/kd-tree/kd-tree-naive.cu
+++ src/kNN/kd-tree/kd-tree-naive.cu
@@ -6,101 +6,39 @@
 #include <helper_cuda.h>
 
 #define checkCudaErrors(val)           check ( (val), #val, __FILE__, __LINE__ )
-// #define THREADS_PER_BLOCK 512U
-// #define MAX_BLOCK_DIM_SIZE 65535U
-#define THREADS_PER_BLOCK 4U
-#define MAX_BLOCK_DIM_SIZE 8U
+#define THREADS_PER_BLOCK 512U
+#define MAX_BLOCK_DIM_SIZE 65535U
+// #define THREADS_PER_BLOCK 4U
+// #define MAX_BLOCK_DIM_SIZE 8U
+
+#include <string.h>
+#define debug 0
+#define FILE (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define debugf(fmt, ...) if(debug){printf("%s:%d: " fmt, FILE, __LINE__, __VA_ARGS__);}
 
 
-__constant__  size_t pitch;
+__constant__  int pitch;
 
 
 
-int index(int i, int j, int n)
+int h_index(int i, int j, int n)
 {
     return i + j * n;
 }
 
-void swap(float *points, int a, int b, int n)
+void h_swap(float *points, int a, int b, int n)
 {
-    float tx = points[index(a, 0, n)],
-    ty = points[index(a, 1, n)],
-    tz = points[index(a, 2, n)];
-    points[index(a, 0, n)] = points[index(b, 0, n)], points[index(b, 0, n)] = tx;
-    points[index(a, 1, n)] = points[index(b, 1, n)], points[index(b, 1, n)] = ty;
-    points[index(a, 2, n)] = points[index(b, 2, n)], points[index(b, 2, n)] = tz;
+    float tx = points[h_index(a, 0, n)],
+    ty = points[h_index(a, 1, n)],
+    tz = points[h_index(a, 2, n)];
+    points[h_index(a, 0, n)] = points[h_index(b, 0, n)], points[h_index(b, 0, n)] = tx;
+    points[h_index(a, 1, n)] = points[h_index(b, 1, n)], points[h_index(b, 1, n)] = ty;
+    points[h_index(a, 2, n)] = points[h_index(b, 2, n)], points[h_index(b, 2, n)] = tz;
 }
 
 int midpoint(int lower, int upper)
 {
     return (int) floor((upper - lower) / 2) + lower;
-}
-
-float quick_select(int k, float *points, int lower, int upper, int dim, int n)
-{
-    int pos, i,
-    left = lower,
-    right = upper - 1;
-
-    float pivot;
-
-    while (left < right)
-    {
-        pivot = points[index(k, dim, n)];
-        swap(points, k, right, n);
-        for (i = pos = left; i < right; i++)
-        {
-            if (points[index(i, dim, n)] < pivot)
-            {
-                swap(points, i, pos, n);
-                pos++;
-            }
-        }
-        swap(points, right, pos, n);
-        if (pos == k) break;
-        if (pos < k) left = pos + 1;
-        else right = pos - 1;
-    }
-    return points[index(k, dim, n)];
-}
-
-void center_median(float *points, int lower, int upper, int dim, int n)
-{
-    int i, r = midpoint(lower, upper);
-
-    float median = quick_select(r, points, lower, upper, dim, n);
-
-    for (i = lower; i < upper; ++i)
-    {
-        if (points[index(i, dim, n)] == median)
-        {
-            swap(points, i, r, n);
-            return;
-        }
-    }
-}
-
-void balance_branch(float *points, int lower, int upper, int dim, int n)
-{
-    if (lower >= upper) return;
-
-    int i, r = midpoint(lower, upper);
-
-    center_median(points, lower, upper, dim, n);
-
-    upper--;
-
-    for (i = lower; i < r; ++i)
-    {
-        if (points[index(i, dim, n)] > points[index(r, dim, n)])
-        {
-            while (points[index(upper, dim, n)] > points[index(r, dim, n)])
-            {
-                upper--;
-            }
-            swap(points, i, upper, n);
-        }
-    }
 }
 
 __device__ __host__
@@ -116,152 +54,198 @@ unsigned int nextPowerOf2(unsigned int x)
 }
 
 
-__device__
-int index(int i, int j)
+__device__ int index(int i, int j, int offset)
 {
-    return i + j * pitch;
+    return i + (j * pitch) + offset;
+}
+
+__device__
+bool isPowTwo(unsigned int x)
+{
+  return ((x&(x-1))==0);
+}
+
+__device__
+unsigned int prevPowerOf2(unsigned int n){
+    if (isPowTwo(n))
+    {
+        return n;
+    }
+    n = nextPowerOf2(n);
+    return n >>=1;
+
 }
 
 
-__device__
-void printMatrix(float* points, int n){
-#if __CUDA_ARCH__>=200
-
-    if (threadIdx.x ==0)
+void h_print_matrix(float* points, int n){
+    if (debug)
     {
-    /* code */
+        printf("#################\n");
         for (int i = 0; i < n; ++i)
         {
-            printf("i = %3d", i );
+            printf("i = %2d:   ", i );
             for (int j = 0; j < 3; ++j)
             {
-                printf(  " %3.1f ", points[index(i,j)] );
+                printf(  " %5.0f ", points[h_index(i,j,n)] );
             }
-            printf("\n" );
+            printf("\n");
         }
+    }
+}
+
+__device__
+void printMatrix(float* points, int n, int offset){
+#if __CUDA_ARCH__>=200
+    if (debug)
+    {
+        __syncthreads();
+        if (threadIdx.x ==0 && blockIdx.x ==0)
+        {
+            printf("####################\n");
+            printf("block = %d, blockOffset = %d\n", blockIdx.x, offset );
+            for (int i = 0; i < n; ++i)
+            {
+                printf("i = %3d", i );
+                for (int j = 0; j < 3; ++j)
+                {
+                    printf(  " %3.1f ", points[index(i,j, offset)] );
+                }
+                printf("\n",1 );
+            }
+            printf("\n####################\n");
+        }
+        __syncthreads();
     }
 #endif
 }
 
 __device__
-void swap(float *points, int a, int b)
+void swap(float *points, int a, int b, int offset)
 {
-    float tx = points[index(a, 0)],
-    ty = points[index(a, 1)],
-    tz = points[index(a, 2)];
-    points[index(a, 0)] = points[index(b, 0)], points[index(b, 0)] = tx;
-    points[index(a, 1)] = points[index(b, 1)], points[index(b, 1)] = ty;
-    points[index(a, 2)] = points[index(b, 2)], points[index(b, 2)] = tz;
+    float tx = points[index(a, 0, offset)],
+    ty = points[index(a, 1, offset)],
+    tz = points[index(a, 2, offset)];
+    points[index(a, 0, offset)] = points[index(b, 0, offset)], points[index(b, 0, offset)] = tx;
+    points[index(a, 1, offset)] = points[index(b, 1, offset)], points[index(b, 1, offset)] = ty;
+    points[index(a, 2, offset)] = points[index(b, 2, offset)], points[index(b, 2, offset)] = tz;
 }
 
 
 __device__
-void cuCompare(float* points, int a, int b, int ddd, int n, int dir){
+void cuCompare(float* points, int a, int b, int ddd, int n, int offset, int dir){
     if (b < n)
     {
-        #if __CUDA_ARCH__>=200
-        // printf("block/Thread: %d/%d, swap:%1d,  %4d <--> %4d #### %3.1f  <--> %3.1f\n", blockIdx.x, threadIdx.x, (points[index(a,dir)] >=points[index(b,dir)]) == ddd, a, b, points[index(a, 0)], points[index(b, 0)]  );
-        #endif
-        if ((points[index(a,dir)] >=points[index(b,dir)]) == ddd)
+        if ((points[index(a,dir,offset)] >= points[index(b,dir,offset)]) == ddd)
         {
-            swap(points, a, b);
+            swap(points, a, b, offset);
         }
     }
 }
 
-__global__
-void cuBalanceBranch(float* points, int n, int numBranch, int dir){
-
-// int blockoffset = blockIdx.x * blockDim.x *2;
-// dist+=blockoffset;
-// ind+=blockoffset;
-
-
-    unsigned int size, m, i, stride, ddd, pos,
-    tid = threadIdx.x;
+__device__
+void cuBitonicSortOneBlock(float *points, unsigned int n, unsigned int offset, int dir)
+{
+    unsigned int size, stride, ddd, pos, tid;
+    unsigned int i, m;
 
     for (size = 2; size <= nextPowerOf2(n); size <<= 1)
     {
         tid=threadIdx.x;
         ddd = 1 ^ ((tid & (size / 2)) != 0);
-
-        if (size > n)
-        {
-            i = tid;
-            m=size;
-            m >>=1;
-            while(i < n-m)
-            {
-                ddd = 1 ^ ((tid & (size / 2)) != 0);
-                cuCompare(points, i, m-i-1, !ddd, n, dir);
-                i+=blockDim.x;
-            }
-        }
-
-        __syncthreads();
         for (stride = size / 2; stride > 0; stride >>= 1)
         {
             tid = threadIdx.x;
             __syncthreads();
-            while(tid < n/2){
+            while(tid < n/2)
+            {
                 ddd = 1 ^ ((tid & (size / 2)) != 0);
                 pos = 2 * tid - (tid & (stride - 1));
-                cuCompare(points, pos, pos+stride, ddd, n, dir);
+                cuCompare(points, pos, pos+stride, ddd, n, offset, dir);
                 tid+=blockDim.x;
             }
         }
     }
 }
 
+
+
+// A shorted bitonic sort is used to find and place the median.
+__global__
+void cuBalanceBranch(float* points, int n, unsigned int p, int dir){
+
+    unsigned int prev_pow_two, i, step, tid, blockoffset, bid;
+    tid = threadIdx.x;
+    bid = blockIdx.x;
+    while(bid < p){
+        step = (int) (n / p);
+        blockoffset = (step) * bid;
+        n=step;
+        prev_pow_two = prevPowerOf2(n);
+        printMatrix(points, prev_pow_two, blockoffset);
+        cuBitonicSortOneBlock(points, n, blockoffset, dir);
+        // if (!isPowTwo(n))
+        // {
+        // __syncthreads();
+        //     printMatrix(points, n, blockoffset);
+        //     while(tid < (n-prev_pow_two))
+        //     {
+        //         swap(points, prev_pow_two-tid, prev_pow_two+tid, blockoffset);
+        //         tid+=blockDim.x;
+        //     }
+        //     __syncthreads();
+        //     cuBitonicSortOneBlock(points, n, blockoffset, dir);
+        //     __syncthreads();
+        //     while(tid < (n-prev_pow_two))
+        //     {
+        //         swap(points, prev_pow_two-tid, prev_pow_two+tid, blockoffset);
+        //         tid+=blockDim.x;
+        //     }
+        //     __syncthreads();
+        //     cuBitonicSortOneBlock(points, prev_pow_two, (n-prev_pow_two)+blockoffset, dir);
+        //     printMatrix(points, n, blockoffset);
+        // }
+        bid += gridDim.x;
+    }
+
+}
+
 void getThreadAndBlockCount(int n, int p, int &blocks, int &threads)
 {
     blocks = min(MAX_BLOCK_DIM_SIZE, p);
-    threads = min(THREADS_PER_BLOCK, n/2);
+    blocks = max(1, blocks);
+    threads = min(THREADS_PER_BLOCK, (n/p)/2);
 }
 
 void build_kd_tree(float *h_points, int n)
 {
-    float* d_points;
-    int step, h,p, numBlocks, numThreads,
-    dim = 3;
-    size_t d_pitch_in_bytes,d_pitch, h_pitch_in_bytes = n*sizeof(float);
 
-    h = ceil(log2((float)n + 1) - 1);
+    h_print_matrix(h_points, n);
+
+    float* d_points;
+    int p, h, i, numBlocks, numThreads, d_pitch, dim = 3;
+    size_t d_pitch_in_bytes, h_pitch_in_bytes = n*sizeof(float);
+
 
     checkCudaErrors(
         cudaMallocPitch(&d_points, &d_pitch_in_bytes, n*sizeof(float), dim));
     d_pitch    = d_pitch_in_bytes/sizeof(float);
-    checkCudaErrors(cudaMemcpyToSymbol(pitch, &d_pitch, sizeof(size_t)));
 
+    checkCudaErrors(cudaMemcpyToSymbol(pitch, &d_pitch, sizeof(int)));
 
     checkCudaErrors(
         cudaMemcpy2D(d_points, d_pitch_in_bytes, h_points, h_pitch_in_bytes, n*sizeof(float), dim, cudaMemcpyHostToDevice));
-    p=1;
-    getThreadAndBlockCount(n, p, numBlocks, numThreads);
-    cuBalanceBranch<<<numBlocks,numThreads>>>(d_points, n, 1, 0);
+    h = ceil(log2((float)n + 1) - 1);
+    for (i = 0; i < h; i++)
+    {
+        p = pow(2, i);
+        getThreadAndBlockCount(n, p, numBlocks, numThreads);
+        debugf("n = %d, p = %d, numblosck = %d, numThread =%d\n", n, p, numBlocks, numThreads );
+        cuBalanceBranch<<<numBlocks,numThreads>>>(d_points, n, p, i%3);
+    }
 
     checkCudaErrors(
         cudaMemcpy2D(h_points, h_pitch_in_bytes, d_points, d_pitch_in_bytes, n*sizeof(float), dim, cudaMemcpyDeviceToHost));
-
-    // printf("#################\n");
-    // for (int i = 0; i < n; ++i)
-    // {
-    //     printf("i = %2d:   ", i );
-    //     for (int j = 0; j < 3; ++j)
-    //     {
-    //         printf(  " %5.0f ", h_points[index(i,j,n)] );
-    //     }
-    //     printf("\n" );
-    // }
-    // for (i = 0; i < h; ++i)
-    // {
-    //     step = (int) floor(n / pow(2, i)) + 1;
-    //     for (j = 0; j < n; j+=step)
-    //     {
-    //         balance_branch(h_points, j, j+step-1, i%dim, n);
-    //     }
-    // }
-
+    h_print_matrix(h_points, n);
     checkCudaErrors(cudaFree(d_points));
 }
 
--- tests/kNN/kd-tree/test-kd-tree-naive.cu
+++ tests/kNN/kd-tree/test-kd-tree-naive.cu
@@ -6,143 +6,152 @@
 #include "gtest/gtest.h"
 
 
-
-
+#define debug 0
 
 void print_tree(float *tree, int level, int lower, int upper, int n)
 {
-	if (lower >= upper)
-	{
-		return;
-	}
-
-	int i, r = midpoint(lower, upper);
-
-	printf("|");
-	for (i = 0; i < level; ++i)
-	{
-		printf("--");
-	}
-	printf("(%3.1f, %3.1f, %3.1f)\n", tree[index(r, 0, n)], tree[index(r, 1, n)], tree[index(r, 2, n)]);
-
-	print_tree(tree, 1 + level, lower, r, n);
-	print_tree(tree, 1 + level, r + 1, upper, n);
-}
-
-TEST(kd_tree_naive, kd_tree_naive_correctness){
-	int debug = 0;
-	int i, j, n = 15;
-	float *points, *expected_points;
-	points = (float*) malloc(n * 3 * sizeof(float));
-	expected_points = (float*) malloc(n * 3 * sizeof(float));
-
-	for ( i = 0; i < n; ++i)
-	{
-		for ( j = 0; j < 3; ++j)
-		{
-			points[index(i, j, n)] = n - i -1;
-			expected_points[index(i, j, n)] = i ;
-		}
-	}
-
-	build_kd_tree(points, n);
-
-	for ( i = 0; i < n; ++i)
-	{
-		for ( j = 0; j < 3; ++j)
-		{
-			ASSERT_EQ(points[index(i, j, n)] ,i) << "Faild with i = " << i << " j = " <<j ;
-		}
-	}
-
-	if (debug)
-	{
-
-		printf("expected points:\n");
-		print_tree(expected_points, 0, 0, n, n);
-		printf("==================\n");
-
-		printf("kd tree:\n");
-		print_tree(points, 0, 0, n, n);
-		printf("==================\n");
-
-	}
-
-	free(points);
-	free(expected_points);
-}
-
-TEST(kd_tree_naive, kd_tree_naive_step_correctness){
-  int debug = 0;
-  int i, j, n;
-
-  for (int n = 16; n <= 100; n++)
+  if (debug)
   {
-    float *points, *expected_points;
-    points = (float*) malloc(n * 3 * sizeof(float));
-    srand(time(NULL));
-    for ( i = 0; i < n; ++i)
+    if (lower >= upper)
     {
-      for ( j = 0; j < 3; ++j)
-      {
-        points[index(i, j, n)] = (float)rand()/100000000;
-      }
+      return;
     }
 
-    build_kd_tree(points, n);
+    int i, r = midpoint(lower, upper);
 
-    for ( i = 0; i < n/2; ++i)
+    printf("|");
+    for (i = 0; i < level; ++i)
     {
-      ASSERT_LE(points[index(i,0,n)], points[index(i+n/2,0,n)]) << "Faild with n = "<< n<< " i = " << i  ;
+      printf("--");
     }
+    printf("(%3.1f, %3.1f, %3.1f)\n", tree[h_index(r, 0, n)], tree[h_index(r, 1, n)], tree[h_index(r, 2, n)]);
 
-    if (debug)
-    {
-      printf("kd tree:\n");
-      print_tree(points, 0, 0, n, n);
-      printf("==================\n");
-    }
-
-    free(points);
+    print_tree(tree, 1 + level, lower, r, n);
+    print_tree(tree, 1 + level, r + 1, upper, n);
   }
 }
 
-  // float *ref, *dist;
-  // float *query;
-  // int *ind;
-  // unsigned int    ref_nb = 131072;
-  // unsigned int    query_nb = 1;
-  // unsigned int    dim=3;
-  // unsigned int    k          = 100;
-  // unsigned int    iterations = 1;
-  // unsigned int    i;
+TEST(kd_tree_naive, kd_tree_naive_correctness){
+  int i, j, n = 8388608;
+  float *points, *expected_points;
+  points = (float*) malloc(n * 3 * sizeof(float));
+  expected_points = (float*) malloc(n * 3 * sizeof(float));
+  srand(time(NULL));
+  for ( i = 0; i < n; ++i)
+  {
+    for ( j = 0; j < 3; ++j)
+    {
+      points[h_index(i, j, n)] = n - i -1 +j;
+      points[h_index(i, j, n)] = (float) rand() /100000000;
+      expected_points[h_index(i, j, n)] = i ;
+    }
+  }
+  if (debug)
+  {
 
-  // ref    = (float *) malloc(ref_nb   * dim * sizeof(float));
-  // query  = (float *) malloc(query_nb * dim * sizeof(float));
-  // dist  = (float *) malloc(k * sizeof(float));
-  // ind  = (int *) malloc(k * sizeof(float));
 
-  // for (unsigned int count = 0; count < ref_nb*dim; count++)
-  // {
-  //   ref[count] = (float)ref_nb*dim-count;
-  // }
-  // for (unsigned int count = 0; count < query_nb*dim; count++)
-  // {
-  //   query[count] = 0;
-  // }
+    printf("kd tree:\n");
+    print_tree(points, 0, 0, n, n);
+    printf("==================\n");
 
-  // for (i=0; i<iterations; i++){
-  //   knn_brute_force_reduce(ref, ref_nb, query, dim, k, dist, ind);
-  // }
+  }
 
-  // for (unsigned int i = 0; i < k; ++i)
-  // {
-  //   ASSERT_EQ(ind[i], ref_nb-1-i) << "Faild with i = "<<i << " and n = " << ref_nb;;
-  // }
+  build_kd_tree(points, n);
 
-  // free(dist);
-  // free(ind);
-  // free(query);
-  // free(ref);
-  // cudaDeviceSynchronize();
-  // cudaDeviceReset();
+  for ( i = 0; i < n; ++i)
+  {
+    for ( j = 0; j < 3; ++j)
+    {
+// ASSERT_EQ(points[h_index(i, j, 5n)] ,i) << "Faild with i = " << i << " j = " <<j ;
+    }
+  }
+
+  if (debug)
+  {
+
+    printf("kd tree:\n");
+    print_tree(points, 0, 0, n, n);
+    printf("==================\n");
+
+  }
+
+  free(points);
+  free(expected_points);
+}
+
+TEST(kd_tree_naive, kd_tree_naive_timeing)
+{
+  int i, j, n = 8388608;
+  float *points;
+  points = (float*) malloc(n * 3 * sizeof(float));
+  srand(time(NULL));
+  for ( i = 0; i < n; ++i)
+  {
+    for ( j = 0; j < 3; ++j)
+    {
+      points[h_index(i, j, n)] = (float) rand() /100000000;
+    }
+  }
+
+  cudaEvent_t start, stop;
+  unsigned int bytes = n*3 * (sizeof(float));
+  checkCudaErrors(cudaEventCreate(&start));
+  checkCudaErrors(cudaEventCreate(&stop));
+  float elapsed_time=0;
+
+  checkCudaErrors(cudaEventRecord(start, 0));
+
+  build_kd_tree(points, n);
+
+  checkCudaErrors(cudaEventRecord(stop, 0));
+  cudaEventSynchronize(start);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&elapsed_time, start, stop);
+  elapsed_time = elapsed_time ;
+  double throughput = 1.0e-9 * ((double)bytes)/(elapsed_time* 1e-3);
+  printf("build_kd_tree_naive, Throughput = %.4f GB/s, Time = %.5f ms, Size = %u Elements, NumDevsUsed = %d\n",
+    throughput, elapsed_time, n, 1);
+
+  free(points);
+}
+
+
+// float *ref, *dist;
+// float *query;
+// int *ind;
+// unsigned int    ref_nb = 131072;
+// unsigned int    query_nb = 1;
+// unsigned int    dim=3;
+// unsigned int    k          = 100;
+// unsigned int    iterations = 1;
+// unsigned int    i;
+
+// ref    = (float *) malloc(ref_nb   * dim * sizeof(float));
+// query  = (float *) malloc(query_nb * dim * sizeof(float));
+// dist  = (float *) malloc(k * sizeof(float));
+// ind  = (int *) malloc(k * sizeof(float));
+
+// for (unsigned int count = 0; count < ref_nb*dim; count++)
+// {
+//   ref[count] = (float)ref_nb*dim-count;
+// }
+// for (unsigned int count = 0; count < query_nb*dim; count++)
+// {
+//   query[count] = 0;
+// }
+
+// for (i=0; i<iterations; i++){
+//   knn_brute_force_reduce(ref, ref_nb, query, dim, k, dist, ind);
+// }
+
+// for (unsigned int i = 0; i < k; ++i)
+// {
+//   ASSERT_EQ(ind[i], ref_nb-1-i) << "Faild with i = "<<i << " and n = " << ref_nb;;
+// }
+
+// free(dist);
+// free(ind);
+// free(query);
+// free(ref);
+// cudaDeviceSynchronize();
+// cudaDeviceReset();
