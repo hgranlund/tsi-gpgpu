166ffcd5463ce43478f8e2dcd9a02afe880e7e7d
Teodor Ande Elstad <teodoran@stud.ntnu.no>
2014-03-03 13:15:19 +0100

    Iterative converted to float arrays.

--- src/kNN/kd-tree/kd-tree-iterative.c
+++ src/kNN/kd-tree/kd-tree-iterative.c
@@ -106,12 +106,28 @@ void build_kd_tree(float *x, int n)
 
         for (j = 0; j < p; ++j)
         {
-            balance_branch(x, (1 + step) * j, step * (1 + j), i%3, n);
+            balance_branch(x, (1 + step) * j, step * (1 + j), i % 3, n);
         }
     }
     return;
 }
 
+float euclid_distance(float ax, float ay, float az, float bx, float by, float bz)
+{
+    return sqrt(pow((ax - bx), 2.0) + pow((ay - by), 2.0) + pow((az - bz), 2.0));
+}
+
+int closest_point(float *qp, float *tree, int a, int b, int n)
+{
+    float a_distance = euclid_distance(qp[0], qp[1], qp[2], tree[ind(a, 0, n)], tree[ind(a, 1, n)], tree[ind(a, 2, n)]),
+        b_distance = euclid_distance(qp[0], qp[1], qp[2], tree[ind(b, 0, n)], tree[ind(b, 1, n)], tree[ind(b, 2, n)]);
+
+    if (a_distance < b_distance)
+    {
+        return a;
+    }
+    return b;
+}
 
 // Rewrite for new data structure.
 // void nearest(struct kd_node_t *root, struct kd_node_t *nd, int i, int dim, struct kd_node_t **best, double *best_dist)
@@ -138,6 +154,24 @@ void build_kd_tree(float *x, int n)
 //     if (dx2 >= *best_dist) return;
 //     nearest(dx > 0 ? root->right : root->left, nd, i, dim, best, best_dist);
 // }
+
+int nearest(float *qp, float *tree, int lower, int upper, int n)
+{
+    if (lower >= upper)
+    {
+        return lower;
+    }
+
+
+    int r = midpoint(lower, upper),
+        left_best = nearest(qp, tree, lower, r, n),
+        right_best = nearest(qp, tree, r + 1, upper, n);
+
+    printf("(%3.1f, %3.1f, %3.1f)\n", tree[ind(r, 0, n)], tree[ind(r, 1, n)], tree[ind(r, 2, n)]);
+    printf("r = %d\n", r);
+
+    return closest_point(qp, tree, left_best, right_best, n);
+}
 
 void print_tree(float *tree, int level, int lower, int upper, int n)
 {
@@ -168,7 +202,7 @@ double WallTime()
 
 int main(int argc, char *argv[])
 {
-    int i, j, n = 1000000, wn = 6, debug = 0;
+    int i, j, n = 10, wn = 6, debug = 1;
     float *points, *wiki;
 
     points = (float*) malloc(n * 3 * sizeof(float));
@@ -191,16 +225,15 @@ int main(int argc, char *argv[])
 
     if (debug)
     {
-        wiki = (float*) malloc(n * 3* sizeof(float*));
+        wiki = (float*) malloc(wn * 3 * sizeof(float*));
 
         // (2,3), (5,4), (9,6), (4,7), (8,1), (7,2).
-        wiki[ind(0, 0, n)] = 2, wiki[ind(0, 1, n)] = 3, wiki[ind(0, 2, n)] = 0;
-        wiki[ind(1, 0, n)] = 5, wiki[ind(1, 1, n)] = 4, wiki[ind(1, 2, n)] = 0;
-        wiki[ind(2, 0, n)] = 9, wiki[ind(2, 1, n)] = 6, wiki[ind(2, 2, n)] = 0;
-        wiki[ind(3, 0, n)] = 4, wiki[ind(3, 1, n)] = 7, wiki[ind(3, 2, n)] = 0;
-        wiki[ind(4, 0, n)] = 8, wiki[ind(4, 1, n)] = 1, wiki[ind(4, 2, n)] = 0;
-        wiki[ind(5, 0, n)] = 7, wiki[ind(5, 1, n)] = 2, wiki[ind(5, 2, n)] = 0;
-
+        wiki[ind(0, 0, wn)] = 2, wiki[ind(0, 1, wn)] = 3, wiki[ind(0, 2, wn)] = 0;
+        wiki[ind(1, 0, wn)] = 5, wiki[ind(1, 1, wn)] = 4, wiki[ind(1, 2, wn)] = 0;
+        wiki[ind(2, 0, wn)] = 9, wiki[ind(2, 1, wn)] = 6, wiki[ind(2, 2, wn)] = 0;
+        wiki[ind(3, 0, wn)] = 4, wiki[ind(3, 1, wn)] = 7, wiki[ind(3, 2, wn)] = 0;
+        wiki[ind(4, 0, wn)] = 8, wiki[ind(4, 1, wn)] = 1, wiki[ind(4, 2, wn)] = 0;
+        wiki[ind(5, 0, wn)] = 7, wiki[ind(5, 1, wn)] = 2, wiki[ind(5, 2, wn)] = 0;
 
         printf("initial list:\n");
         print_tree(points, 0, 0, n, n);
@@ -220,7 +253,20 @@ int main(int argc, char *argv[])
         print_tree(wiki, 0, 0, wn, wn);
         printf("==================\n");
 
+        float query_point[] = {0, 0, 0};
+        int best_fit = nearest(query_point, wiki, 0, wn, wn);
+        printf("Closest point to (%3.1f, %3.1f, %3.1f) was (%3.1f, %3.1f, %3.1f) located at %d\n",
+            query_point[0], query_point[1], query_point[2],
+            wiki[ind(best_fit, 0, wn)], wiki[ind(best_fit, 1, wn)], wiki[ind(best_fit, 2, wn)],
+            best_fit);
+        printf("==================\n");
+
         free(wiki);
+
+        printf("euclid_distance test:\n");
+        printf("Should be 3: %f\n", euclid_distance(2, 2, 1, 0, 0, 0));
+        printf("Should be 3: %f\n", euclid_distance(4, -2, 1, 2, -4, 0));
+        printf("==================\n");
     }
     free(points);
     return 0;
