8e20d4ad38426d70bda1ad05de1fee735ec84f00
Simen Haugerud Granlund <simen@hgranlund.com>
2014-03-11 22:09:39 +0100

    made test nice a little bit smaller

--- src/kNN/kernels/common.cuh
+++ src/kNN/kernels/common.cuh
@@ -31,35 +31,4 @@ unsigned int prevPowTwo(unsigned int n)
     return n >>=1;
 }
 
-
-__device__ int cuSumReduce(int *list, int n)
-{
-  int half = n/2;
-  int tid = threadIdx.x;
-  while(tid<half && half > 0)
-  {
-    list[tid] += list[tid+half];
-    half = half/2;
-}
-return list[0];
-}
-
-//TODO must be imporved
-__device__  void cuAccumulateIndex(int *list, int n)
-{
-    if (threadIdx.x == 0)
-    {
-        int sum=0;
-        list[n]=list[n-1];
-        int temp=0;
-        for (int i = 0; i < n; ++i)
-        {
-            temp = list[i];
-            list[i] = sum;
-            sum += temp;
-        }
-        list[n]+=list[n-1];
-    }
-}
-
 #endif
--- tests/kNN/kd-tree/test-kd-tree-naive.cu
+++ tests/kNN/kd-tree/test-kd-tree-naive.cu
@@ -116,7 +116,8 @@ __host__  void h_printPointsArray__(Point *l, int n, char *s, int l_debug=0)
 
   TEST(kd_tree_naive, kd_tree_naive_time){
     int i, n = 8388608;
-    for (n = 13388608; n <=14000000 ; n+=250000)
+    for (n = 8388608; n <=8388608 ; n+=250000)
+    // for (n = 13388608; n <=14000000 ; n+=250000)
     {
       cudaDeviceReset();
       float temp;
