031bec8716d2c30ca59c12a083a252f144fa5832
Teodor Ande Elstad <teodoran@stud.ntnu.no>
2014-04-02 12:01:51 +0200

    Started integrating new search.

--- src/kNN/kernels/kd-search.cu
+++ src/kNN/kernels/kd-search.cu
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <math.h>
+#include <float.h>
 
 #include <helper_cuda.h>
 
@@ -22,10 +23,162 @@ int store_locations(Point *tree, int lower, int upper, int n)
     return r;
 }
 
+// __device__
+// float dist(Point qp, Point *points, int x)
+// {
+//     float dx = qp.p[0] - points[x].p[0],
+//           dy = qp.p[1] - points[x].p[1],
+//           dz = qp.p[2] - points[x].p[2];
+
+//     return dx * dx + dy * dy + dz * dz;
+// }
+
+// __device__
+// void push(int *stack, int *eos, int value)
+// {
+//     (*eos)++;
+//     stack[*eos] = value;
+// }
+
+// __device__
+// int pop(int *stack, int *eos)
+// {
+//     if (*eos > -1)
+//     {
+//         (*eos)--;
+//         return stack[*eos + 1];
+//     }
+//     else
+//     {
+//         return -1;
+//     }
+// }
+
+// __device__
+// int peek(int *stack, int eos)
+// {
+//     if (eos > -1)
+//     {
+//         return stack[eos];
+//     }
+//     else
+//     {
+//         return -1;
+//     }
+// }
+
+// __device__
+// void upDim(int *dim)
+// {
+//     if (*dim >= 2)
+//     {
+//         (*dim) = 0;
+//     }
+//     else
+//     {
+//         (*dim)++;
+//     }
+// }
+
+// __device__
+// void downDim(int *dim)
+// {
+//     if (*dim <= 0)
+//     {
+//         (*dim) = 2;
+//     }
+//     else
+//     {
+//         (*dim)--;
+//     }
+// }
+
+// __device__
+// int nn(Point qp, Point *tree, int *stack, int n, int k)
+// {
+//     int eos = -1,
+//         // *stack = (int *) malloc(2 * log2((float) n) * sizeof stack),
+
+//          dim = 0,
+//          best,
+
+//          previous = -1,
+//          current,
+//          target,
+//          other;
+
+//     float best_dist = FLT_MAX,
+//           current_dist;
+
+//     push(stack, &eos, n / 2);
+//     upDim(&dim);
+
+//     while (eos > -1)
+//     {
+//         current = peek(stack, eos);
+//         target = tree[current].left;
+//         other = tree[current].right;
+
+//         current_dist = dist(qp, tree, current);
+
+//         if (current_dist < best_dist)
+//         {
+//             best_dist = current_dist;
+//             best = current;
+//         }
+
+//         if (qp.p[dim] > tree[current].p[dim])
+//         {
+//             int temp = target;
+
+//             target = other;
+//             other = temp;
+//         }
+
+//         if (previous == target)
+//         {
+//             if (other > -1 && (tree[current].p[dim] - qp.p[dim]) * (tree[current].p[dim] - qp.p[dim]) < best_dist)
+//             {
+//                 push(stack, &eos, other);
+//                 upDim(&dim);
+//             }
+//             else
+//             {
+//                 pop(stack, &eos);
+//             }
+//         }
+//         else if (previous == other)
+//         {
+//             pop(stack, &eos);
+//         }
+//         else
+//         {
+//             if (target > -1)
+//             {
+//                 push(stack, &eos, target);
+//                 upDim(&dim);
+//             }
+//             else if (other > -1)
+//             {
+//                 push(stack, &eos, other);
+//                 upDim(&dim);
+//             }
+//             else
+//             {
+//                 pop(stack, &eos);
+//             }
+//         }
+
+//         previous = current;
+//     }
+
+//     return best;
+// }
+
 __device__
 int nn(Point qp, Point *tree, int n, int k)
 {
-    return 1;
+    return 0;
 }
 
 __global__
