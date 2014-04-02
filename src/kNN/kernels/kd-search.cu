ae780a2a93f5487e6ddd9fc9ddeb99611f726fac
Simen Haugerud Granlund <simen@hgranlund.com>
2014-04-02 12:48:29 +0200

    merged search and search sceleton

--- src/kNN/kernels/kd-search.cu
+++ src/kNN/kernels/kd-search.cu
@@ -23,171 +23,174 @@ int store_locations(Point *tree, int lower, int upper, int n)
     return r;
 }
 
-// __device__
-// float dist(Point qp, Point *points, int x)
-// {
-//     float dx = qp.p[0] - points[x].p[0],
-//           dy = qp.p[1] - points[x].p[1],
-//           dz = qp.p[2] - points[x].p[2];
-
-//     return dx * dx + dy * dy + dz * dz;
-// }
-
-// __device__
-// void push(int *stack, int *eos, int value)
-// {
-//     (*eos)++;
-//     stack[*eos] = value;
-// }
-
-// __device__
-// int pop(int *stack, int *eos)
-// {
-//     if (*eos > -1)
-//     {
-//         (*eos)--;
-//         return stack[*eos + 1];
-//     }
-//     else
-//     {
-//         return -1;
-//     }
-// }
-
-// __device__
-// int peek(int *stack, int eos)
-// {
-//     if (eos > -1)
-//     {
-//         return stack[eos];
-//     }
-//     else
-//     {
-//         return -1;
-//     }
-// }
-
-// __device__
-// void upDim(int *dim)
-// {
-//     if (*dim >= 2)
-//     {
-//         (*dim) = 0;
-//     }
-//     else
-//     {
-//         (*dim)++;
-//     }
-// }
-
-// __device__
-// void downDim(int *dim)
-// {
-//     if (*dim <= 0)
-//     {
-//         (*dim) = 2;
-//     }
-//     else
-//     {
-//         (*dim)--;
-//     }
-// }
-
-// __device__
-// int nn(Point qp, Point *tree, int *stack, int n, int k)
-// {
-//     int eos = -1,
-//         // *stack = (int *) malloc(2 * log2((float) n) * sizeof stack),
-
-//          dim = 0,
-//          best,
-
-//          previous = -1,
-//          current,
-//          target,
-//          other;
-
-//     float best_dist = FLT_MAX,
-//           current_dist;
-
-//     push(stack, &eos, n / 2);
-//     upDim(&dim);
-
-//     while (eos > -1)
-//     {
-//         current = peek(stack, eos);
-//         target = tree[current].left;
-//         other = tree[current].right;
-
-//         current_dist = dist(qp, tree, current);
-
-//         if (current_dist < best_dist)
-//         {
-//             best_dist = current_dist;
-//             best = current;
-//         }
-
-//         if (qp.p[dim] > tree[current].p[dim])
-//         {
-//             int temp = target;
-
-//             target = other;
-//             other = temp;
-//         }
-
-//         if (previous == target)
-//         {
-//             if (other > -1 && (tree[current].p[dim] - qp.p[dim]) * (tree[current].p[dim] - qp.p[dim]) < best_dist)
-//             {
-//                 push(stack, &eos, other);
-//                 upDim(&dim);
-//             }
-//             else
-//             {
-//                 pop(stack, &eos);
-//             }
-//         }
-//         else if (previous == other)
-//         {
-//             pop(stack, &eos);
-//         }
-//         else
-//         {
-//             if (target > -1)
-//             {
-//                 push(stack, &eos, target);
-//                 upDim(&dim);
-//             }
-//             else if (other > -1)
-//             {
-//                 push(stack, &eos, other);
-//                 upDim(&dim);
-//             }
-//             else
-//             {
-//                 pop(stack, &eos);
-//             }
-//         }
-
-//         previous = current;
-//     }
-
-//     return best;
-// }
-
 __device__
-int nn(Point qp, Point *tree, int n, int k)
+float cuDist(Point qp, Point *points, int x)
 {
-    return 0;
+    float dx = qp.p[0] - points[x].p[0],
+          dy = qp.p[1] - points[x].p[1],
+          dz = qp.p[2] - points[x].p[2];
+
+    return dx * dx + dy * dy + dz * dz;
 }
 
+__device__
+void cuPush(int *stack, int *eos, int value)
+{
+    (*eos)++;
+    stack[*eos] = value;
+}
+
+__device__
+int cuPop(int *stack, int *eos)
+{
+    if (*eos > -1)
+    {
+        (*eos)--;
+        return stack[*eos + 1];
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+__device__
+int cuPeek(int *stack, int eos)
+{
+    if (eos > -1)
+    {
+        return stack[eos];
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+__device__
+void cuUpDim(int *dim)
+{
+    if (*dim >= 2)
+    {
+        (*dim) = 0;
+    }
+    else
+    {
+        (*dim)++;
+    }
+}
+
+__device__
+void cuDownDim(int *dim)
+{
+    if (*dim <= 0)
+    {
+        (*dim) = 2;
+    }
+    else
+    {
+        (*dim)--;
+    }
+}
+
+__device__
+int cuSearch(Point qp, Point *tree, int *stack, int n, int k)
+{
+    int eos = -1,
+        // *stack = (int *) malloc(2 * log2((float) n) * sizeof stack),
+
+        dim = 0,
+        best,
+
+        previous = -1,
+        current,
+        target,
+        other;
+
+    float best_dist = FLT_MAX,
+          current_dist;
+
+    cuPush(stack, &eos, n / 2);
+    cuUpDim(&dim);
+
+    while (eos > -1)
+    {
+        current = cuPeek(stack, eos);
+        target = tree[current].left;
+        other = tree[current].right;
+
+        current_dist = cuDist(qp, tree, current);
+
+        if (current_dist < best_dist)
+        {
+            best_dist = current_dist;
+            best = current;
+        }
+
+        if (qp.p[dim] > tree[current].p[dim])
+        {
+            int temp = target;
+
+            target = other;
+            other = temp;
+        }
+
+        if (previous == target)
+        {
+            if (other > -1 && (tree[current].p[dim] - qp.p[dim]) * (tree[current].p[dim] - qp.p[dim]) < best_dist)
+            {
+                cuPush(stack, &eos, other);
+                cuUpDim(&dim);
+            }
+            else
+            {
+                cuPop(stack, &eos);
+                cuDownDim(&dim);
+            }
+        }
+        else if (previous == other)
+        {
+            cuPop(stack, &eos);
+            cuDownDim(&dim);
+        }
+        else
+        {
+            if (target > -1)
+            {
+                cuPush(stack, &eos, target);
+                cuUpDim(&dim);
+            }
+            else if (other > -1)
+            {
+                cuPush(stack, &eos, other);
+                cuUpDim(&dim);
+            }
+            else
+            {
+                cuPop(stack, &eos);
+                cuDownDim(&dim);
+            }
+        }
+
+        previous = current;
+    }
+
+    return best;
+}
+
+template <int thread_stack_size>
 __global__
 void dQueryAll(Point *query_points, Point *tree, int n_qp, int n_tree, int k, int *result)
 {
     int tid = threadIdx.x,
         rest = n_qp % gridDim.x,
         block_step = n_qp / gridDim.x,
+        *l_stack,
         block_offset = block_step * blockIdx.x;
+
+    __shared__ int stack[thread_stack_size * THREADS_PER_BLOCK_SEARCH];
+    l_stack = stack;
+    l_stack += thread_stack_size * threadIdx.x;
 
     if (rest >= gridDim.x - blockIdx.x)
     {
@@ -198,14 +201,14 @@ void dQueryAll(Point *query_points, Point *tree, int n_qp, int n_tree, int k, in
     // printf("blockIdx: %d, threadIdx: %d, gridDim: %d, blockDim: %d\n", blockIdx.x, threadIdx.x, gridDim.x, blockDim.x);
     while (tid < block_step)
     {
-        result[tid] = nn(query_points[tid], tree, n_tree, k);
+        result[tid] = cuSearch(query_points[tid], tree, stack, n_tree, k);
         tid += blockDim.x;
     }
 }
 
 void getThreadAndBlockCountForQueryAll(int n, int &blocks, int &threads)
 {
-    threads = 128;
+    threads = THREADS_PER_BLOCK_SEARCH;
     blocks = n / threads;
     blocks = min(MAX_BLOCK_DIM_SIZE, blocks);
     blocks = max(1, blocks);
@@ -225,7 +228,7 @@ void queryAll(Point *h_query_points, Point *h_tree, int n_qp, int n_tree, int k,
     checkCudaErrors(cudaMemcpy(d_tree, h_tree, n_tree * sizeof(Point), cudaMemcpyHostToDevice));
 
     getThreadAndBlockCountForQueryAll(n_qp, numBlocks, numThreads);
-    dQueryAll <<< numBlocks, numThreads>>>(d_tree, d_tree, n_qp, n_tree, k, d_result);
+    dQueryAll<200> <<< numBlocks, numThreads>>>(d_tree, d_tree, n_qp, n_tree, k, d_result);
 
     checkCudaErrors(cudaMemcpy(h_result, d_result, n_qp * k * sizeof(int), cudaMemcpyDeviceToHost));
     checkCudaErrors(cudaFree(d_tree));
