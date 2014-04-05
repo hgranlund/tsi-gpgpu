b6b5bda0135f0e2d0bd50624e34534295ebd9474
Simen Haugerud Granlund <simen@hgranlund.com>
2014-04-05 17:17:33 +0200

    small naming changee

--- include/knn_gpgpu.h
+++ include/knn_gpgpu.h
@@ -6,7 +6,7 @@
 void knn_brute_force_garcia(float *ref_host, int ref_width, float *query_host, int query_width, int height, int k, float *dist_host, int *ind_host);
 void knn_brute_force(float *ref_host, int ref_nb, float *query_host, int dim, int k, float *dist_host, int *ind_host);
 
-void build_kd_tree(struct PointS *h_points, int n, struct Point *h_points_out);
+void build_kd_tree(struct PointS *points, int n, struct Point *tree);
 void queryAll(struct Point *query_points, struct Point *tree, int n_qp, int n_tree, int k, int *result);
 
 
