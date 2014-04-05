3ad2d847a5f621fbe2c0aaeb749f528955f46183
Simen Haugerud Granlund <simen@hgranlund.com>
2014-04-05 15:59:20 +0200

    added build tree and query all to gpgpu- library

--- include/knn_gpgpu.h
+++ include/knn_gpgpu.h
@@ -1,8 +1,13 @@
 #ifndef _KERNELS_H_
 #define _KERNELS_H_
+#include "point.h"
 
-void knn_brute_force_garcia(float* ref_host, int ref_width, float* query_host, int query_width, int height, int k, float* dist_host, int* ind_host);
-void knn_brute_force(float* ref_host, int ref_nb, float* query_host, int dim, int k, float* dist_host, int* ind_host);
+
+void knn_brute_force_garcia(float *ref_host, int ref_width, float *query_host, int query_width, int height, int k, float *dist_host, int *ind_host);
+void knn_brute_force(float *ref_host, int ref_nb, float *query_host, int dim, int k, float *dist_host, int *ind_host);
+
+void build_kd_tree(struct PointS *h_points, int n, struct Point *h_points_out);
+void queryAll(struct Point *query_points, struct Point *tree, int n_qp, int n_tree, int k, int *result);
 
 
 #endif //  _KERNELS_H_
