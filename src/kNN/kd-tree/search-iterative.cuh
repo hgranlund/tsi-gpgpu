28d7f35479ea3440b0ab9ab949f6bf78c375f46b
Simen Haugerud Granlund <simen@hgranlund.com>
2014-04-08 13:49:14 +0200

    Refactored kd-search-iterative and tests.

--- src/kNN/kd-tree/search-iterative.cuh
+++ src/kNN/kd-tree/search-iterative.cuh
@@ -2,8 +2,6 @@
 #define _SEARCH_ITERATIVE_
 #include <point.h>
 
-int cashe_indexes(Point *tree, int lower, int upper, int n);
-int dfs(Point *tree, int n);
 void push(int *stack, int *pop, int value);
 int pop(int *stack, int *eos);
 int peek(int *stack, int eos);
@@ -11,6 +9,5 @@ int find(int *stack, int eos, int value);
 void upDim(int *dim);
 void downDim(int *dim);
 int query_a(float *qp, Point *tree, int n);
-int query_k(float *qp, Point *tree, int dim, int index);
 
 #endif
--- tests/kNN/kd-tree/test-search-iterative.cu
+++ tests/kNN/kd-tree/test-search-iterative.cu
@@ -7,162 +7,11 @@
 #include <helper_cuda.h>
 #include "gtest/gtest.h"
 
-
-
-int store_locations_(Point *tree, int lower, int upper, int n)
-{
-    int r;
-
-    if (lower >= upper)
-    {
-        return -1;
-    }
-
-    r = (int) ((upper - lower) / 2) + lower;
-
-    tree[r].left = store_locations_(tree, lower, r, n);
-    tree[r].right = store_locations_(tree, r + 1, upper, n);
-
-    return r;
-}
-
-
-void _swap(struct Point *points, int a, int b)
-{
-    struct Point t = points[a];
-    points[a] = points[b], points[b] = t;
-}
-
-int _midpoint(int lower, int upper)
-{
-    return (int) ((upper - lower) / 2) + lower;
-}
-
-float _quick_select(int k, struct Point *x, int lower, int upper, int dim)
-{
-    int pos, i,
-        left = lower,
-        right = upper - 1;
-
-    float pivot;
-
-    while (left < right)
-    {
-        pivot = x[k].p[dim];
-        _swap(x, k, right);
-        for (i = pos = left; i < right; i++)
-        {
-            if (x[i].p[dim] < pivot)
-            {
-                _swap(x, i, pos);
-                pos++;
-            }
-        }
-        _swap(x, right, pos);
-        if (pos == k) break;
-        if (pos < k) left = pos + 1;
-        else right = pos - 1;
-    }
-    return x[k].p[dim];
-}
-
-void _center_median(struct Point *x, int lower, int upper, int dim)
-{
-    int i, r = _midpoint(lower, upper);
-
-    float median = _quick_select(r, x, lower, upper, dim);
-
-    for (i = lower; i < upper; ++i)
-    {
-        if (x[i].p[dim] == median)
-        {
-            _swap(x, i, r);
-        }
-    }
-}
-
-void _balance_branch(struct Point *x, int lower, int upper, int dim)
-{
-    if (lower >= upper) return;
-
-    int i, r = _midpoint(lower, upper);
-
-    _center_median(x, lower, upper, dim);
-
-    upper--;
-
-    for (i = lower; i < r; ++i)
-    {
-        if (x[i].p[dim] > x[r].p[dim])
-        {
-            while (x[upper].p[dim] > x[r].p[dim])
-            {
-                upper--;
-            }
-            _swap(x, i, upper);
-        }
-    }
-
-    // To enable direct recursive execution.
-    // _balance_branch(x, lower, r, 0);
-    // _balance_branch(x, r + 1, upper, 0);
-}
-
-void _build_kd_tree(struct Point *x, int n)
-{
-    int i, j, p, step,
-        h = ceil(log2((float) (n + 1)) - 1);
-    for (i = 0; i < h; ++i)
-    {
-        p = (int) pow(2.0, i);
-        step = (int) (n / p);
-
-        for (j = 0; j < p; ++j)
-        {
-            _balance_branch(x, (1 + step) * j, step * (1 + j), i % 3);
-        }
-    }
-    return;
-}
-
-void print_t(Point *tree, int level, int lower, int upper, int n)
-{
-    if (lower >= upper)
-    {
-        return;
-    }
-
-    int i, r = ((upper - lower) / 2) + lower;
-
-    printf("|");
-    for (i = 0; i < level; ++i)
-    {
-        printf("--");
-    }
-    printf("(%3.1f, %3.1f, %3.1f)\n", tree[r].p[0], tree[r].p[1], tree[r].p[2]);
-
-    print_t(tree, 1 + level, lower, r, n);
-    print_t(tree, 1 + level, r + 1, upper, n);
-}
-
-void _printPointsArray(Point *l, int n, char *s)
-{
-    printf("%10s: [ ", s);
-    for (int i = 0; i < n; ++i)
-    {
-        printf("%3.1f, ", l[i].p[0]);
-    }
-    printf("]\n");
-}
-
 bool isExpectedPoint(struct Point *tree, int n, float qx, float qy, float qz, float ex, float ey, float ez)
 {
-    // float dists[n];
-
     float query_point[3];
     query_point[0] = qx, query_point[1] = qy, query_point[2] = qz;
 
-    // // int best_fit = nn(query_point, tree, dists, 0, _midpoint(0, n));
     int best_fit = query_a(query_point, tree, n);
 
     float actual = tree[best_fit].p[0] + tree[best_fit].p[1] + tree[best_fit].p[2];
@@ -177,154 +26,35 @@ bool isExpectedPoint(struct Point *tree, int n, float qx, float qy, float qz, fl
 
 TEST(search_iterative, search_iterative_wiki_correctness)
 {
-    int wn = 6;
-    struct Point *wiki = (Point *) malloc(wn  * sizeof(Point));
+    int n = 6;
 
+    struct PointS *points = (PointS *) malloc(n  * sizeof(PointS));
+    struct Point *points_out = (Point *) malloc(n  * sizeof(Point));
 
-    // (2,3), (5,4), (9,6), (4,7), (8,1), (7,2).
-    wiki[0].p[0] = 2, wiki[0].p[1] = 3, wiki[0].p[2] = 0;
-    wiki[1].p[0] = 5, wiki[1].p[1] = 4, wiki[1].p[2] = 0;
-    wiki[2].p[0] = 9, wiki[2].p[1] = 6, wiki[2].p[2] = 0;
-    wiki[3].p[0] = 4, wiki[3].p[1] = 7, wiki[3].p[2] = 0;
-    wiki[4].p[0] = 8, wiki[4].p[1] = 1, wiki[4].p[2] = 0;
-    wiki[5].p[0] = 7, wiki[5].p[1] = 2, wiki[5].p[2] = 0;
+    points[0].p[0] = 2, points[0].p[1] = 3, points[0].p[2] = 0;
+    points[1].p[0] = 5, points[1].p[1] = 4, points[1].p[2] = 0;
+    points[2].p[0] = 9, points[2].p[1] = 6, points[2].p[2] = 0;
+    points[3].p[0] = 4, points[3].p[1] = 7, points[3].p[2] = 0;
+    points[4].p[0] = 8, points[4].p[1] = 1, points[4].p[2] = 0;
+    points[5].p[0] = 7, points[5].p[1] = 2, points[5].p[2] = 0;
 
-    // cudaDeviceReset();
-    _build_kd_tree(wiki, wn);
-    cashe_indexes(wiki, 0, wn, wn);
+    cudaDeviceReset();
+    build_kd_tree(points, n, points_out);
 
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 2, 3, 0, 2, 3, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 5, 4, 0, 5, 4, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 9, 6, 0, 9, 6, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 4, 7, 0, 4, 7, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 8, 1, 0, 8, 1, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 7, 2, 0, 7, 2, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 10, 10, 0, 9, 6, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 0, 0, 0, 2, 3, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 4, 4, 0, 5, 4, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 3, 2, 0, 2, 3, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 2, 6, 0, 4, 7, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 10, 0, 0, 8, 1, 0));
-    ASSERT_EQ(true, isExpectedPoint(wiki, wn, 0, 10, 0, 4, 7, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 2, 3, 0, 2, 3, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 5, 4, 0, 5, 4, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 9, 6, 0, 9, 6, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 4, 7, 0, 4, 7, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 8, 1, 0, 8, 1, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 7, 2, 0, 7, 2, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 10, 10, 0, 9, 6, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 0, 0, 0, 2, 3, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 4, 4, 0, 5, 4, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 3, 2, 0, 2, 3, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 2, 6, 0, 4, 7, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 10, 0, 0, 8, 1, 0));
+    ASSERT_EQ(true, isExpectedPoint(points_out, n, 0, 10, 0, 4, 7, 0));
 }
-
-// TEST(kd_search, kd_search_timing_cpu)
-// {
-//     int i, n = 1000000;
-//     PointS *points;
-//     Point *points_out;
-//     points = (PointS *) malloc(n  * sizeof(PointS));
-//     points_out = (Point *) malloc(n  * sizeof(Point));
-//     srand(time(NULL));
-
-//     for (i = 0; i < n; ++i)
-//     {
-//         PointS t;
-//         t.p[0] = rand();
-//         t.p[1] = rand();
-//         t.p[2] = rand();
-//         points[i] = t;
-//     }
-
-//     cudaDeviceReset();
-
-
-
-//     build_kd_tree(points, n, points_out);
-
-
-//     store_locations_(points_out, 0, n, n);
-
-//     int test_runs = 1;
-//     float **query_data = (float **) malloc(test_runs * sizeof * query_data);
-
-//     for (i = 0; i < test_runs; i++)
-//     {
-//         query_data[i] = (float *) malloc(3 * sizeof * query_data[i]);
-//         query_data[i][0] = rand() % 1000;
-//         query_data[i][1] = rand() % 1000;
-//         query_data[i][2] = rand() % 1000;
-//     }
-
-//     cudaEvent_t start, stop;
-//     unsigned int bytes = n * (sizeof(Point) + sizeof(int));
-//     checkCudaErrors(cudaEventCreate(&start));
-//     checkCudaErrors(cudaEventCreate(&stop));
-//     float elapsed_time = 0;
-
-//     checkCudaErrors(cudaEventRecord(start, 0));
-
-//     for (i = 0; i < test_runs; i++)
-//     {
-//         query_a(query_data[i], points_out,  n);
-//     }
-
-
-//     checkCudaErrors(cudaEventRecord(stop, 0));
-//     cudaEventSynchronize(start);
-//     cudaEventSynchronize(stop);
-//     cudaEventElapsedTime(&elapsed_time, start, stop);
-//     elapsed_time = elapsed_time ;
-//     double throughput = 1.0e-9 * ((double)bytes) / (elapsed_time * 1e-3);
-//     printf("query_a, Throughput = %.4f GB/s, Time = %.5f ms, Size = %u Elements, NumDevsUsed = %d\n",
-//            throughput, elapsed_time, n, 1);
-
-
-//     for (i = 0; i < test_runs; ++i)
-//     {
-//         free(query_data[i]);
-//     }
-//     free(query_data);
-
-//     free(points);
-// }
-
-// TEST(search_iterative, search_iterative_dfs)
-// {
-//     int wn = 6;
-//     struct Point *wiki = (Point *) malloc(wn  * sizeof(Point));
-
-//     // (2,3), (5,4), (9,6), (4,7), (8,1), (7,2).
-//     wiki[0].p[0] = 2, wiki[0].p[1] = 3, wiki[0].p[2] = 0;
-//     wiki[1].p[0] = 5, wiki[1].p[1] = 4, wiki[1].p[2] = 0;
-//     wiki[2].p[0] = 9, wiki[2].p[1] = 6, wiki[2].p[2] = 0;
-//     wiki[3].p[0] = 4, wiki[3].p[1] = 7, wiki[3].p[2] = 0;
-//     wiki[4].p[0] = 8, wiki[4].p[1] = 1, wiki[4].p[2] = 0;
-//     wiki[5].p[0] = 7, wiki[5].p[1] = 2, wiki[5].p[2] = 0;
-
-//     // cudaDeviceReset();
-//     _build_kd_tree(wiki, wn);
-//     print_t(wiki, 0, 0, wn, wn);
-//     printf("\n");
-
-//     cashe_indexes(wiki, 0, wn, wn);
-
-//     dfs(wiki, wn);
-// }
-
-// TEST(search_iterative, search_iterative_query_a)
-// {
-//     int wn = 6;
-//     struct Point *wiki = (Point *) malloc(wn  * sizeof(Point)),
-//                   *qp = (Point *) malloc(sizeof(Point));
-
-//     // (2,3), (5,4), (9,6), (4,7), (8,1), (7,2).
-//     wiki[0].p[0] = 2, wiki[0].p[1] = 3, wiki[0].p[2] = 0;
-//     wiki[1].p[0] = 5, wiki[1].p[1] = 4, wiki[1].p[2] = 0;
-//     wiki[2].p[0] = 9, wiki[2].p[1] = 6, wiki[2].p[2] = 0;
-//     wiki[3].p[0] = 4, wiki[3].p[1] = 7, wiki[3].p[2] = 0;
-//     wiki[4].p[0] = 8, wiki[4].p[1] = 1, wiki[4].p[2] = 0;
-//     wiki[5].p[0] = 7, wiki[5].p[1] = 2, wiki[5].p[2] = 0;
-
-//     // cudaDeviceReset();
-//     _build_kd_tree(wiki, wn);
-//     print_t(wiki, 0, 0, wn, wn);
-//     printf("\n");
-
-//     cashe_indexes(wiki, 0, wn, wn);
-
-//     query_a(qp, wiki, wn);
-// }
 
 TEST(search_iterative, search_iterative_push)
 {
