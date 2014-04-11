4ddf3635e39aaf3dee350b77315268a8ad398123
Simen Haugerud Granlund <simen@hgranlund.com>
2014-04-11 11:22:24 +0200

    added memory outprints in search and build

--- src/kNN/kd-tree/kd-tree-build.cu
+++ src/kNN/kd-tree/kd-tree-build.cu
@@ -151,6 +151,38 @@ void build_kd_tree(struct PointS *h_points, int n, struct Point *h_points_out)
         i++;
     }
 
+
+    // show memory usage of GPU
+
+    size_t free_byte ;
+
+    size_t total_byte ;
+
+    cudaError_t cuda_status = cudaMemGetInfo( &free_byte, &total_byte ) ;
+
+    if ( cudaSuccess != cuda_status )
+    {
+
+        printf("Error: cudaMemGetInfo fails, %s \n", cudaGetErrorString(cuda_status) );
+
+        exit(1);
+
+    }
+
+
+
+    double free_db = (double)free_byte ;
+
+    double total_db = (double)total_byte ;
+
+    double used_db = total_db - free_db ;
+
+    printf("GPU memory usage: used = %f, free = %f MB, total = %f MB\n", used_db / 1024.0 / 1024.0, free_db / 1024.0 / 1024.0, total_db / 1024.0 / 1024.0);
+
+
+
+
+
     checkCudaErrors(cudaFree(d_swap));
     checkCudaErrors(cudaFree(d_partition));
     checkCudaErrors(cudaFree(d_steps));
