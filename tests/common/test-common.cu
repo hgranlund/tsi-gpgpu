bb6b9df65bca68ae4e6629de6b14ba80df12de18
Simen Haugerud Granlund <simen@hgranlund.com>
2014-04-25 12:39:01 +0200

    added readPoints in test-common

--- tests/common/test-common.cu
+++ tests/common/test-common.cu
@@ -1,4 +1,5 @@
 #include "test-common.cuh"
+#include "math.h"
 
 void populatePoints(struct Point *points, int n)
 {
@@ -34,6 +35,21 @@ void populatePointSs(struct PointS *points, int n)
     }
 }
 
+#define rand1() (rand() / (double)RAND_MAX)
+
+void populatePointSRosetta(struct PointS *points, int n)
+{
+    int i;
+    srand(time(NULL));
+
+    for (i = 0; i < n; ++i)
+    {
+        struct PointS t;
+        t.p[0] = rand1(), t.p[1] = rand1(), t.p[2] = rand1();
+        points[i] = t;
+    }
+}
+
 void cudaStartTiming(cudaEvent_t &start, cudaEvent_t &stop, float &elapsed_time)
 {
     elapsed_time = 0;
@@ -55,6 +71,26 @@ void printCudaTiming(float elapsed_time, float bytes, int n)
     double throughput = 1.0e-9 * ((double)bytes) / (elapsed_time * 1e-3);
 
     printf("Throughput = %.4f GB/s, Time = %.5f ms, Size = %u Elements\n", throughput, elapsed_time, n);
+}
+
+void readPoints(const char *file_path, int n, struct PointS *points)
+{
+    FILE *file = fopen(file_path, "rb");
+    if (file == NULL)
+    {
+        fputs ("File error\n", stderr);
+        exit (1);
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        fread(&points[i].p, sizeof(float), 3, file);
+        for (int j = 0; j < 3; ++j)
+        {
+            points[i].p[j] = round(points[i].p[j] / 100000000.0);
+        }
+    }
+
+    fclose(file);
 }
 
 void ASSERT_TREE_EQ(struct Point *expected_tree, struct Point *actual_tree, int n)
@@ -81,12 +117,12 @@ void ASSERT_TREE_LEVEL_OK(PointS *points, int *steps, int n, int p)
 
         for (int i = 0; i < n / 2; ++i)
         {
-            ASSERT_LE(t_points[i].p[0], t_points[n / 2].p[0]) << "Faild with n = " << n << " and p " << p;
+            ASSERT_LE(t_points[i].p[0], t_points[n / 2].p[0]) << "Faild with n = " << n << " and p " << p << " at i = " << i;
         }
 
         for (int i = n / 2; i < n; ++i)
         {
-            ASSERT_GE(t_points[i].p[0], t_points[n / 2].p[0]) << "Faild with n = " << n << " and p " << p;
+            ASSERT_GE(t_points[i].p[0], t_points[n / 2].p[0]) << "Faild with n = " << n << " and p " << p << " at i =" << i;
         }
     }
 }
--- tests/common/test-common.cuh
+++ tests/common/test-common.cuh
@@ -11,9 +11,13 @@
 
 void populatePoints(struct Point *points, int n);
 void populatePointSs(struct PointS *points, int n);
+void populatePointSRosetta(struct PointS *points, int n);
+
 void cudaStartTiming(cudaEvent_t &start, cudaEvent_t &stop, float &elapsed_time);
 void cudaStopTiming(cudaEvent_t &start, cudaEvent_t &stop, float &elapsed_time);
 void printCudaTiming(float elapsed_time, float bytes, int n);
+
+void readPoints(const char *file_path, int n, struct PointS *points);
 
 void ASSERT_TREE_EQ(struct Point *expected_tree, struct Point *actual_tree, int n);
 void ASSERT_TREE_LEVEL_OK(PointS *points, int *steps, int n, int p);
