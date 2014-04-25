3fceb1bcdf7bd26bd19adff232c28a81a6b187ff
Simen Haugerud Granlund <simen@hgranlund.com>
2014-04-25 12:33:21 +0200

    renamed max block and thread size definition in radix

--- src/kNN/kd-tree/radix-select.cuh
+++ src/kNN/kd-tree/radix-select.cuh
@@ -3,8 +3,8 @@
 #include "point.h"
 
 
-#define THREADS_PER_BLOCK 1024U
-#define MAX_BLOCK_DIM_SIZE 65535U
+#define THREADS_PER_BLOCK_RADIX 512
+#define MAX_BLOCK_DIM_SIZE_RADIX 65535U
 #define MAX_SHARED_MEM 49152U
 
 void radixSelectAndPartition(struct PointS *points, struct PointS *swap, int *partition, int n, int dir);
