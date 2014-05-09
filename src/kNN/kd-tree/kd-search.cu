7b614195cd101f01a7c8dba5265d18e28f39e678
Simen Haugerud Granlund <simen@hgranlund.com>
2014-05-09 12:09:21 +0000

    added a fastIntegerLog2 method

--- src/kNN/kd-tree/kd-search.cu
+++ src/kNN/kd-tree/kd-search.cu
@@ -162,6 +162,17 @@ void cuKNN(struct Point qp, struct Node *tree, int n, int k, int *result,
     }
 }
 
+__device__ int fastIntegerLog2(int x)
+{
+    int y = 0;
+    while (x > 2)
+    {
+        y++;
+        x >>= 1;
+    }
+    return y;
+}
+
 __device__ void cuCalculateBlockOffsetAndNoOfQueries(int n, int &n_per_block, int &block_offset)
 {
     int rest = n % gridDim.x;
@@ -186,7 +197,7 @@ __global__
 void dQueryAll(struct Point *query_points, struct Node *tree, int n_qp, int n_tree, int k, int *result)
 {
     int tid = threadIdx.x,
-        stack_size = log2((float)n_tree) * 2,
+        stack_size = fastIntegerLog2(n_tree) * 2,
         block_step,
         block_offset;
 
