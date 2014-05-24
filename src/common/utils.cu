130d47b6e523970ccc185e6b5c2ad293c6a7d23a
Simen Haugerud Granlund <simen@hgranlund.com>
2014-05-24 11:58:05 +0000

    added stream utils functiosn in utils.h

--- src/common/utils.cu
+++ src/common/utils.cu
@@ -4,8 +4,41 @@
 #include "knn_gpgpu.h"
 
 
-
 void cuSetDevice(int devive)
 {
     checkCudaErrors(cudaSetDevice(devive));
 }
+void cuGetDevice(int *devive)
+{
+    checkCudaErrors(cudaGetDevice(devive));
+}
+
+void cuGetDeviceCount(int *device_count)
+{
+    checkCudaErrors(cudaGetDeviceCount(device_count));
+}
+
+void cuStreamCreate(cudaStream_t *pStream, int device)
+{
+    int device_orig;
+    cuGetDevice(&device_orig);
+    cuSetDevice(device);
+    checkCudaErrors(cudaStreamCreate(pStream));
+    cuSetDevice(device_orig);
+}
+
+void cuStreamSynchronize(cudaStream_t stream)
+{
+    checkCudaErrors(cudaStreamSynchronize(stream));
+}
+
+void cuStreamDestroy(cudaStream_t stream)
+{
+    checkCudaErrors(cudaStreamDestroy(stream));
+}
+
+void cuGetDeviceProperties(struct cudaDeviceProp *prop, int device)
+{
+    cudaGetDeviceProperties(prop, device);
+}
+
--- src/common/utils.cuh
+++ src/common/utils.cuh
@@ -1,6 +1,13 @@
 #ifndef _CU_UTILS_
 #define _CU_UTILS_
+#include <cuda_runtime.h>
 
 void cuSetDevice(int devive);
+void cuGetDevice(int *devive);
+void cuGetDeviceCount(int *device_count);
+void cuStreamCreate(cudaStream_t *pStream, int device);
+void cuStreamSynchronize(cudaStream_t stream);
+void cuStreamDestroy(cudaStream_t stream);
+void cuGetDeviceProperties(struct cudaDeviceProp *prop, int device);
 
 #endif
